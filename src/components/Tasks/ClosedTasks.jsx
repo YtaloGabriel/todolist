97a6a801837f7e41c9b0aa6cdfd88ffd95757b8d
Ytalo Gabriel <ytalogabr@gmail.com>
2022-07-26 14:15:34 -0300

    close tasks on click ok

--- src/components/Tasks/ClosedTasks.jsx
+++ src/components/Tasks/ClosedTasks.jsx
@@ -1,7 +1,39 @@
 import React from 'react';
+import TaskItem from './TaskItem';
+import Styles from '../css/Tasks/Tasks.module.css';
 
-const ClosedTasks = () => {
-  return <div>ClosedTasks</div>;
+const ClosedTasks = ({ data }) => {
+  const closedTasksRender = () => {
+    return (
+      <section className={Styles.container}>
+        <div className={Styles.contentTop}>
+          <h3 className={Styles.title}>
+            Tarefas Fechadas - <span>({data.length})</span>
+          </h3>
+        </div>
+
+        {data.length &&
+          data.map(({ taskId, taskState, taskName, taskDate, taskCate }) => {
+            return (
+              <TaskItem
+                key={taskId}
+                id={taskId}
+                state={taskState}
+                name={taskName}
+                date={taskDate}
+                category={taskCate}
+              />
+            );
+          })}
+      </section>
+    );
+  };
+
+  if (data.length) {
+    return closedTasksRender();
+  } else {
+    return null;
+  }
 };
 
 export default ClosedTasks;
--- src/components/Tasks/TaskItem.jsx
+++ src/components/Tasks/TaskItem.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import { GlobalContext } from '../../GlobalContext';
 import Styles from '../css/Tasks/TaskItem.module.css';
 
 const TaskItem = ({ id, state, name, date, category }) => {
-  console.log(id);
+  const { tasksList, setTasksList } = React.useContext(GlobalContext);
+
+  const toggleTasks = ({ target }) => {
+    const itemId = target.parentElement.id;
+
+    const changeTask = tasksList.filter((i) => {
+      if (i.taskId === itemId && i.taskState === 'opened') {
+        i.taskState = 'closed';
+      } else if (i.taskId === itemId && i.taskState === 'closed') {
+        i.taskState = 'opened';
+      }
+      return i;
+    });
+
+    setTasksList([...changeTask, changeTask]);
+  };
 
   return (
-    <section className={Styles.container}>
-      <div className={Styles.checkBox}></div>
+    <section className={Styles.container} id={id}>
+      <div className={Styles.checkBox} onClick={toggleTasks}></div>
       <div className={Styles.info}>
         <h4 className={Styles.infoTitle}>{name}</h4>
         {category && <span className={Styles.infoCategory}>{category}</span>}
